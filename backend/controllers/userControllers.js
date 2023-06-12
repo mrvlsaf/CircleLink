82a511a8dbc00f4831f5d87ce72a26dc6c40f666
Devraj Singh <dsdevrajsingh55555@gmail.com>
2023-06-12 22:54:11 +0530

    bug fix

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -67,7 +67,7 @@ const authUser = asyncHandler(async (req, res) => {
 
   const user = await User.findOne({ email });
 
-  if (user && (await user.matchPassword(password))) {
+  if (user && (user.matchPassword(password))) {
     res.json({
       _id: user._id,
       name: user.name,
